bf8724d683da302ec73c62df0fd60598076d2f2b
brejnev1917 <brejnev1917@gmail.com>
2022-08-16 17:27:06 +0200

    added jar

--- src/test/java/ru/netology/web/CardBlankTestComplexElementsV1.java
+++ src/test/java/ru/netology/web/CardBlankTestComplexElementsV1.java
@@ -1,47 +1,78 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.conditions.SelectedText;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.pseudo;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class CardBlankTestComplexElementsV1 {
+
+class BankServiceTest {
     @BeforeEach
-    void open() {
-        Selenide.open("http://localhost:9999/");
+    void openBrowser() {
+        open("http://localhost:9999/");
     }
 
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    // POSITIVE
+    @Test
+    void shouldSendForm() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
+        form.$("[data-test-id='phone'] input").setValue("+79099876545");
+        form.$("[data-test-id='agreement']").click();
+        form.$("button").click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    // NEGATIVE
+    @Test
+    void shouldNoValidNameWithEmptyField() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='phone'] input").setValue("+79099876545");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
-    void selectionComplexElements() {
-        $("[data-test-id=city] input").setValue("Ка");
-        $$x("//div//span[@class='menu-item__control']").get(1).click();
-        $("[data-test-id=date] .input__icon").click();
-        LocalDate threeDays = LocalDate.now().plusDays(3);
-        LocalDate inputWeek = LocalDate.now().plusDays(7);
-        if (threeDays.getMonthValue() != inputWeek.getMonthValue()) {
-            $("[data-step='1']").click();
-        }
-        $$("tr td").findBy(text(String.valueOf(inputWeek.getDayOfMonth()))).click();
-        $("[data-test-id=name] .input__control").setValue("Александр Иванов-Сидоров");
-        $("[data-test-id=phone] .input__control").setValue("+79991234564");
-        $("[data-test-id=agreement]").click();
-        $x("//button[contains(@class, 'button_view_extra')]").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на " + generateDate(7)));
+    void shouldNoValidPhoneWithEmptyField() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
     }
+
+    @Test
+    void shouldNoSendFormWithEmptyCheckbox() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
+        form.$("[data-test-id='phone'] input").setValue("+79099876545");
+        form.$(".button").click();
+        $("[data-test-id='agreement'].input_invalid").shouldHave(pseudo(":checkbox__text", "color", "rgb(255, 92, 92)"));
+    }
+
+    @Test
+    void shouldNoValidNameWithNoValidData() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("+79099876545");
+        form.$("[data-test-id='phone'] input").setValue("+79099876545");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNoValidPhoneWithNoValidData() {
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
+        form.$("[data-test-id='phone'] input").setValue("Иванов-Петров Иван");
+        form.$("[data-test-id='agreement']").click();
+        form.$(".button").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
 }
